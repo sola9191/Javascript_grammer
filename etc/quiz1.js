48eb5c3c1ffd7541a042dc0635ab85c138462fcc
Sola Ji <sola9191@gmail.com>
2022-05-13 21:52:20 +0900

    git TEST

---
+++ etc/quiz1.js
@@ -0,0 +1,28 @@
+// 카운터를 만들기
+// 0 이상의 값으로 초기화 한 뒤 하나씩 숫자를 증가할 수 있는 카운터를 만들기
+// Counter class 만들기
+
+class Counter {
+    #value;
+    constructor(num){      
+        if( isNaN(num) || num < 0){
+            this.#value = 0;
+        }else {
+            this.#value = num;
+        }
+    }
+
+    get value(){
+        return this.#value;
+    }
+    increment = () => { this.#value +=1; }
+
+
+}
+
+const counter = new Counter (3);
+counter.increment();
+counter.increment();
+counter.increment();
+counter.increment();
+console.log(counter.value);
